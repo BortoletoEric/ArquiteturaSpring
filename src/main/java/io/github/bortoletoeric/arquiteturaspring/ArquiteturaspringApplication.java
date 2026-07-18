308d29f84d1522143c19a05bc08ef9a6f416d6ff
Bortoleto <ericbortoleto_9@outlook.com>
2026-07-18 18:18:42 -0300

    Add H2 database dependency and refactor application startup logic
    - Add `com.h2database:h2` dependency to `pom.xml`.
    - Refactor `ArquiteturaspringApplication` to use `SpringApplicationBuilder` instead of `SpringApplication.run`.
    - Disable the Spring Boot startup banner in the application builder.
    - Configure the application to use the "production" profile by default.
    - Capture the `ConfigurableApplicationContext` from the application builder.

--- src/main/java/io/github/bortoletoeric/arquiteturaspring/ArquiteturaspringApplication.java
+++ src/main/java/io/github/bortoletoeric/arquiteturaspring/ArquiteturaspringApplication.java
@@ -1,13 +1,25 @@
 package io.github.bortoletoeric.arquiteturaspring;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ArquiteturaspringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArquiteturaspringApplication.class, args);
-	}
+    public static void main(String[] args) {
+//		SpringApplication.run(ArquiteturaspringApplication.class, args);
+
+        SpringApplicationBuilder builder =
+                new SpringApplicationBuilder(ArquiteturaspringApplication.class);
+        builder.run(args);
+
+        builder.bannerMode(Banner.Mode.OFF);
+        ConfigurableApplicationContext context = builder.context();
+
+        builder.profiles("production");
+    }
 
 }
